9b219f7d84b52f0eda0e3fba97da2d02277d47ec
Mauricio Bedoya <mauriyi@hotmail.com>
2025-01-27 10:45:49 -0500

    Update config.py

--- saferide-api/config.py
+++ saferide-api/config.py
@@ -1,4 +1,4 @@
 # Archivo de configuración para la clave secreta
 # Esta clave se utiliza para la firma de tokens JWT y debe ser única y secreta
 # Utilizo una clave generada aleatoriamente de 32 caracteres para mayor seguridad
-SECRET_KEY = "eX0w38Vk7WjtB9Nh4s1FzKg3YqG0wHp7Vr2s5Xn9I8zKz3LZ3u"  # Clave secreta generada de manera segura
+SECRET_KEY = ""  # Clave secreta generada de manera segura
